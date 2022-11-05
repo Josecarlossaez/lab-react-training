306994e5b63acc48b0a395b82f9454c021bf4bc0
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2022-11-05 22:24:13 +0100

    iteration 7ok

---
+++ src/components/DriverCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function DriverCard(props) {
+    let fillStart = "★".repeat(Math.round(props.rating)) + "☆".repeat(5-Math.round(props.rating))
+    console.log(props)
+  return (
+    <div style={{display:"flex" ,justifyContent:"center"}}>
+        <div>
+            <img src={props.img} alt="" width="100px"/>
+        </div>
+        <div >
+           <div>
+            <h3>{props.name}</h3>
+            <p>{fillStart}</p>
+            </div>
+           <div>
+            <p>{props.car.model}</p>
+            <p>{props.car.licensePlate}</p>
+
+           </div>
+        </div>
+    </div>
+  )
+}
+
+export default DriverCard
